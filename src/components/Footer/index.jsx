6ff292f6bb5e0ab45172900c5658f8cd241a8381
TheGoumble <3231javiervargas@gmail.com>
2022-10-24 02:05:53 -0400

    humble beginnigs

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+  <footer>
+    <h2>This is the Footer</h2>
+  </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <section>
+      <h2>This is the Home page</h2>
+    </section>
+  )
+}
+
+export default Home

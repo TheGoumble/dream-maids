fe1c37e055268b4716f4c6a9a49f1d3449eb553f
Javier Vargas <3231javiervargas@gmail.com>
2023-02-16 17:00:59 -0500

    further fleshed out the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,67 +1,93 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-
-import facebook from "../../images/facebook-144.png"
-import yelp from "../../images/yelp-144.png"
 import logo from "../../images/logo.png"
 import theGoumble from "../../images/TheGoumble.png"
 import "./footer.css"
+import "./animation.css"
 
 const Footer = () => {
   return (
     <footer>
-      {/* <div id="connect">
-          <a href="https://www.yelp.com/biz/dream-maids-cleaning-services-greenacres">
-          <img src={yelp} alt="Link to Dream maids' yelp page" />
-          </a>
-          <a href="https://www.facebook.com/dreammaidspalmbeach/">
-          <img src={facebook} alt="Link to Dream maids' facebook page" />
-          </a>
-        </div> */}
-
-      <Container>
-
-        <div className="row info">
-          <div className=" col-md-4 col-11">
-            <span>
-              <b>Phone:</b>
-              <a id="links" target="_blank" href="tel:+(561) 287-0358">
-                (561) 287-0358
-              </a>
-            </span>
-            <b>Location:</b>
-            <p>Palm Beach FL</p>
-          </div>
-
-          <div className="col-md-3 col-11">
-            <img id="img" src={logo} alt="Dream Maids Logo" />
-          </div>
-
-          <div className=" col-md-4 col-11">
-            <span>
-              <b>Hours:</b>
-              <p>Monday - Friday, 9:00 am - 6:00 pm</p>
-              <p>Weekends by request</p>
-            </span>
-          </div>
-        </div>
-      </Container>
-
-      <span>
-        <p>&copy;2022 by Dream Maids LLC</p>
-        <span id="powered">
-          <p>Powered by</p>
-          <a href="https://github.com/TheGoumble/dream-maids" target="_blank">
-            <img
-              src={theGoumble}
-              alt="link to The github repo for the dream maids site"
-              id="theg"
+      <div id="effects">
+        <svg
+          class="waves"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 24 150 28"
+          preserveAspectRatio="none"
+          shape-rendering="auto"
+        >
+          <defs>
+            <path
+              id="gentle-wave"
+              d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
             />
-          </a>
+          </defs>
+          <g class="parallax">
+            <use
+              xlinkHref="#gentle-wave"
+              x="48"
+              y="0"
+              fill="rgba(255,255,255,0.7"
+            />
+            <use
+              xlinkHref="#gentle-wave"
+              x="48"
+              y="3"
+              fill="rgba(255,255,255,0.5)"
+            />
+            <use
+              xlinkHref="#gentle-wave"
+              x="48"
+              y="5"
+              fill="rgba(255,255,255,0.3)"
+            />
+            <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+          </g>
+        </svg>
+      </div>
+      <section>
+        <Container>
+          <div className="row info">
+            <div className=" col-md-4 col-11">
+              <span>
+                <b>Phone:</b>
+                <a id="links" target="_blank" href="tel:+(561) 287-0358">
+                  (561) 287-0358
+                </a>
+              </span>
+              <b>Location:</b>
+              <p>Palm Beach FL</p>
+            </div>
+
+            <div id="logo" className="col-md-3 col-11">
+              <img id="img" src={logo} alt="Dream Maids Logo" />
+            </div>
+
+            <div id="hours" className=" col-md-4 col-11">
+              <span>
+                <b>Hours:</b>
+                <p>Monday - Friday, 9:00 am - 6:00 pm</p>
+                <p>Weekends by request</p>
+              </span>
+            </div>
+          </div>
+        </Container>
+
+        <span>
+          <p>&copy;2023 by Dream Maids LLC</p>
+          <span id="powered">
+            <p>Powered by</p>
+            <a href="https://github.com/TheGoumble/dream-maids" target="_blank">
+              <img
+                src={theGoumble}
+                alt="link to The github repo for the dream maids site"
+                id="theg"
+              />
+            </a>
+          </span>
         </span>
-      </span>
+      </section>
     </footer>
   )
 }
--- src/pages/OurServices/index.jsx
+++ src/pages/OurServices/index.jsx
@@ -1,10 +1,38 @@
 import React from "react"
+import "./ourServices.css"
 
 const OurServices = () => {
   return (
     <section>
       <div>
         <h1>Our Services</h1>
+        <div id="options">
+          <span>
+            <h2>Residential</h2>
+            <p>
+              We specializes in Residential cleaning. we have been working with
+              families around the metropolitan area for over 9 years.
+            </p>
+          </span>
+          <span>
+            <h2>Comercial</h2>
+            <p>
+              Keeping your commercial property neat and clean is an integral
+              part of maintaining your facility, improving productivity, and
+              attracting and retaining customers.
+            </p>
+          </span>
+          <span>
+            <h2>Post-Construction</h2>
+            <p>
+              We offer a detailed cleaning and prepares your house for sale.
+              This is completed by a team of our professional house cleaners,
+              and supervised by a manager. Following this detailed clean, we can
+              also provide periodic or regularly scheduled maintenance or touch
+              up cleaning, depending on your needs.
+            </p>
+          </span>
+        </div>
       </div>
     </section>
   )
